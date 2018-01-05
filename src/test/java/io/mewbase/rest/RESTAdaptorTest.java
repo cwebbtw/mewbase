5947b79fe07390aa909f868c00c33cc9b6a0413e
NigeWarren <Nige@zink-digital.com>
2018-01-05 15:43:08 +0000

    Tests default to port 8080

--- src/test/java/io/mewbase/rest/RESTAdaptorTest.java
+++ src/test/java/io/mewbase/rest/RESTAdaptorTest.java
@@ -74,6 +74,7 @@ public class RESTAdaptorTest extends MewbaseTestBase {
 
     public RestServiceAdaptor setUpServer() {
         sequential.lock();
+        RestAssured.baseURI = "http://localhost:8080";
         return RestServiceAdaptor.instance();
     }
 
