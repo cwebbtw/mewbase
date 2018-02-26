741eb952239e37b7ef9271a3ca51e7cdb089223c
NigeWarren <Nige@zink-digital.com>
2018-02-26 16:51:46 +0000

    MultiSink tests

--- src/test/java/io/mewbase/eventsource/MultiEventSinkTest.java
+++ src/test/java/io/mewbase/eventsource/MultiEventSinkTest.java
@@ -1,22 +1,20 @@
 package io.mewbase.eventsource;
 
-import com.typesafe.config.Config;
+
 import io.mewbase.MewbaseTestBase;
 import io.mewbase.bson.BsonObject;
 import org.junit.Test;
 
-import java.util.List;
+
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
+
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+
 
 /**
  * Created by Nige on 26/2/2018.
@@ -32,17 +30,17 @@ public class MultiEventSinkTest extends MewbaseTestBase {
 
         @Override
         public Long publishSync(String channelName, BsonObject event) {
-            return publishSync.incrementAndGet();
+            return publishSync.getAndIncrement();
         }
 
         @Override
         public CompletableFuture<Long> publishAsync(String channelName, BsonObject event) {
-            return CompletableFuture.completedFuture( publishAsync.incrementAndGet() );
+            return CompletableFuture.completedFuture( publishAsync.getAndIncrement() );
         }
 
         @Override
         public void close() {
-            close.incrementAndGet();
+            close.getAndIncrement();
         }
     }
 
@@ -60,7 +58,7 @@ public class MultiEventSinkTest extends MewbaseTestBase {
 
 
     @Test
-    public void testPublishSingleEventMultiply() throws Exception {
+    public void testPublishSingleEventMultiply()  {
 
         final EventSink es1 = new StubEventSink();
         final EventSink es2 = new StubEventSink();
@@ -73,13 +71,40 @@ public class MultiEventSinkTest extends MewbaseTestBase {
         final BsonObject event = new BsonObject().put("data", UUID.randomUUID().toString());
         final Stream<Long> evtNums = mes.publishSync(channelName, event);
 
-        //long size = evtNums.map( (Long l) -> assertEquals(0L, (long)l ) ).count();
-        //assertEquals(2L, size);
+        long size = evtNums.map( (Long l) -> {
+            assertEquals(0L, (long)l );
+            return 1;
+        }  ).count();
+        assertEquals(2L, size);
 
         mes.close();
     }
 
 
+    @Test
+    public void testPublishMultiEventMultiply()  {
+
+        final long numEventSinks = 7;
+        final EventSink[] eventSinks = new EventSink[(int)numEventSinks];
+        for (int i = 0; i < numEventSinks; i++) eventSinks[i] = EventSink.instance();
+
+        final MultiEventSink mes = MultiEventSink.instance(eventSinks);
+        final String channelName = "StubEventChannel";
+
+        final long numEventsToPublish = 16;
+        LongStream.range(0, numEventsToPublish)
+                .mapToObj( l -> {
+                    final BsonObject event = new BsonObject().put("data", UUID.randomUUID().toString());
+                    final CompletableFuture<Stream<Long>> evtNums = (CompletableFuture)mes.publishAsync(channelName, event);
+                    evtNums.thenAccept( (Stream<Long> sl ) ->
+                            assertEquals( sl.map( (Long p) -> {
+                                        assertEquals((long) p, l);
+                                        return 1;
+                                    } ).count(),  numEventSinks ) );
+                    return null;
+                } );
+        mes.close();
+    }
 
 
 
