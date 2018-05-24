1dd35d77fd8b8522cd06ff384366e41c7b406aa3
Kingsley Davies <kings13y@ukmlg0xqhv2r.home>
2018-05-25 00:26:16 +0100

    Updated var naming and corrected minor typo.

--- mewbase-core/src/main/java/io/mewbase/eventsource/EventSource.java
+++ mewbase-core/src/main/java/io/mewbase/eventsource/EventSource.java
@@ -25,13 +25,13 @@ public interface EventSource extends AutoCloseable {
     }
 
     /**
-     * Create an instance of an EventSource given the configuration sul;lplied as a parameter.
+     * Create an instance of an EventSource given the configuration supplied as a parameter.
      * Default is to use the FileEventSource if nothing is supplied in the config.
      * @param config
      * @return Instance of an EventSource, implemented as configured in the configuration.
      */
-    static EventSource instance(Config config) {
-        return CanFactoryFrom.instance(config.getString(factoryConfigPath), config, () -> new FileEventSource(config) );
+    static EventSource instance(Config cfg) {
+        return CanFactoryFrom.instance(cfg.getString(factoryConfigPath), cfg, () -> new FileEventSource(cfg) );
     }
 
     /**
