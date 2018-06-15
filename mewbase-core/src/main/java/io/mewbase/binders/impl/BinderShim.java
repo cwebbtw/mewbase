16e8366fe4fbad8872f73b00110eba71357f3c54
Kingsley Davies <kingley@d-limitd.com>
2018-06-15 17:04:00 +0100

    Update BinderShim.java

--- mewbase-core/src/main/java/io/mewbase/binders/impl/BinderShim.java
+++ mewbase-core/src/main/java/io/mewbase/binders/impl/BinderShim.java
@@ -20,7 +20,7 @@ import java.util.stream.Stream;
 /**
  * Shims are used for two main purposes ...
  *
- * 1) Instrument the Calls to teh underlying class
+ * 1) Instrument the Calls to the underlying class
  * 2) Implement security on a call by call basis possibly wrt Counters and other instrumentation
  */
 
