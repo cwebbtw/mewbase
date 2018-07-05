aa45aad4fd5cd81019e969099bb3f88fabfbc1bd
NigeWarren <Nige@zink-digital.com>
2018-07-05 12:13:40 +0100

    create table with non deprecated builder

--- mewbase-core/src/main/java/io/mewbase/eventsource/impl/hbase/HBaseEventSink.java
+++ mewbase-core/src/main/java/io/mewbase/eventsource/impl/hbase/HBaseEventSink.java
@@ -2,17 +2,33 @@ package io.mewbase.eventsource.impl.hbase;
 
 import io.mewbase.bson.BsonObject;
 import io.mewbase.eventsource.EventSink;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
+
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 
+import static org.apache.hadoop.hbase.TableName.*;
+
 public class HBaseEventSink implements EventSink {
 
     final Connection connection = ConnectionFactory.createConnection();
+    final Admin admin = connection.getAdmin();
+
+    final private TableName table = valueOf("EventTable");
+    final private String colFamily = "ColFamily";
+    // final HTable desc = new HTable(table);
 
     public HBaseEventSink() throws IOException {
+
+        TableDescriptorBuilder tableBuilder = TableDescriptorBuilder.newBuilder(valueOf("EventTable"));
+
+        ColumnFamilyDescriptor cfd = ColumnFamilyDescriptorBuilder.of(colFamily);
+        tableBuilder.setColumnFamily(cfd);
+
+        admin.createTable(tableBuilder.build() );
+
     }
 
     @Override
