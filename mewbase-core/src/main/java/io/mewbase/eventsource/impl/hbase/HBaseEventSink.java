681c27c546f0c695e727a838f56fea53f71737f9
NigeWarren <Nige@zink-digital.com>
2018-07-09 15:03:16 +0100

    Create and write to table for channel

--- mewbase-core/src/main/java/io/mewbase/eventsource/impl/hbase/HBaseEventSink.java
+++ mewbase-core/src/main/java/io/mewbase/eventsource/impl/hbase/HBaseEventSink.java
@@ -5,44 +5,86 @@ import io.mewbase.eventsource.EventSink;
 
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicLong;
 
-import static org.apache.hadoop.hbase.TableName.*;
 
 public class HBaseEventSink implements EventSink {
 
-    final Connection connection = ConnectionFactory.createConnection();
-    final Admin admin = connection.getAdmin();
+    private final static Logger log = LoggerFactory.getLogger(HBaseEventSink.class);
 
-    final private TableName table = valueOf("EventTable");
-    final private String colFamily = "ColFamily";
-    // final HTable desc = new HTable(table);
+    final Connection connection = ConnectionFactory.createConnection();
+
+    final byte[] colFamily = Bytes.toBytes("Events");
+    final byte[] qualifier = Bytes.toBytes("event");
+
+    final Map<String,AtomicLong> seqNums = new HashMap<>();
 
     public HBaseEventSink() throws IOException {
 
-        TableDescriptorBuilder tableBuilder = TableDescriptorBuilder.newBuilder(valueOf("EventTable"));
-
-        ColumnFamilyDescriptor cfd = ColumnFamilyDescriptorBuilder.of(colFamily);
-        tableBuilder.setColumnFamily(cfd);
-
-        admin.createTable(tableBuilder.build() );
-
     }
+
 
     @Override
     public Long publishSync(String channelName, BsonObject event) {
-        return null;
+        try {
+            Table table = ensureTable(channelName);
+            AtomicLong l = seqNums.computeIfAbsent(channelName, s -> new AtomicLong());
+            Put put = new Put(Bytes.toBytes( l.getAndIncrement()) );
+            put.addColumn(colFamily,qualifier,event.encode().getBytes());
+            table.put(put);
+        } catch (Exception exp) {
+            log.error("failed to get table from connection");
+        }
+        return EventSink.SADLY_NO_CONCEPT_OF_A_MESSAGE_NUMBER;
     }
 
     @Override
     public CompletableFuture<Long> publishAsync(String channelName, BsonObject event) {
-        return null;
+        return CompletableFuture.supplyAsync( () -> publishSync( channelName,  event));
     }
+
 
     @Override
     public void close() {
-
+        try {
+            connection.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
+    /**
+     * Check if the table exists and if not then create
+     * @return
+     */
+    private Table ensureTable(final String channelName ) throws IOException  {
+
+        TableName tableName = TableName.valueOf(channelName);
+        final Admin admin = connection.getAdmin();
+        // Todo - Use local hash map to store "live" tables if the client API doesnt
+        if (! admin.tableExists( tableName ) ) {
+            createTable( tableName,  admin);
+        }
+        admin.close();
+        Table table = connection.getTable(tableName);
+        return table;
+    }
+
+
+    private void createTable(final TableName tableName, final Admin admin) throws IOException {
+        TableDescriptorBuilder tableBuilder = TableDescriptorBuilder.newBuilder(tableName);
+        ColumnFamilyDescriptor cfdb = ColumnFamilyDescriptorBuilder.of("Event");
+        tableBuilder.setColumnFamily(cfdb);
+        admin.createTable( tableBuilder.build() );
+    }
+
 }
--- mewbase-core/src/test/java/io/mewbase/eventsource/impl/hbase/HBaseEventSinkTest.java
+++ mewbase-core/src/test/java/io/mewbase/eventsource/impl/hbase/HBaseEventSinkTest.java
@@ -1,10 +1,12 @@
 package io.mewbase.eventsource.impl.hbase;
 
 import io.mewbase.MewbaseTestBase;
+import io.mewbase.bson.BsonObject;
 import io.mewbase.eventsource.EventSink;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.UUID;
 
 
 public class HBaseEventSinkTest extends MewbaseTestBase {
@@ -13,8 +15,21 @@ public class HBaseEventSinkTest extends MewbaseTestBase {
     public void testCreateHBaseEventSink() throws IOException {
 
         EventSink hbSink = new HBaseEventSink();
+        hbSink.close();
 
     }
 
+    @Test  // Requires HBase to be running see mewbase wiki
+    public void testSingleEvent() throws IOException {
+
+        EventSink hbSink = new HBaseEventSink();
+
+        //final String channelName = UUID.randomUUID().toString();
+        final String channelName = "Dennis";
+        final String dataValue = UUID.randomUUID().toString();
+        final BsonObject bsonEvent = new BsonObject().put("data", dataValue);
+        hbSink.publishSync(channelName, bsonEvent);
+        hbSink.close();
+    }
 
 }
